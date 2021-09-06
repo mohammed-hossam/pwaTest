22b3da16043459f74d29e324aba264976104eb79
mohammed-hossam <mohammedhossam7777@gmail.com>
2021-09-06 23:00:26 +0200

    cahe versioning and fallback

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
 console.log('welcome from sw');
-let cachName = 'pwaTest1';
+let currentStaticCacheName = 'pwaTest-static-v2';
+let currentDynamicCacheName = 'pwaTest-dynamic-v1';
+
 //lazm nktb / w index.html 3shan fl server el etnan msh wa7d
 let assets = [
   '/',
   '/pwa%20test/index.html',
+  '/pwa%20test/offlineFallback.html',
   '/pwa%20test/app.js',
   '/pwa%20test/style.css',
 ];
@@ -13,7 +16,7 @@ self.addEventListener('install', (e) => {
   //console.log('sw installed', e);
   e.waitUntil(
     caches
-      .open(cachName)
+      .open(currentStaticCacheName)
       .then((cache) => {
         cache.addAll(assets).catch((err) => {
           console.log(err);
@@ -28,7 +31,26 @@ self.addEventListener('install', (e) => {
 //activate
 self.addEventListener('activate', (e) => {
   //console.log('sw activated', e);
+  // The keys() method of the Cache interface returns a Promise that resolves to an array of Cache keys.
+
+  //handelling cache versioning
+  e.waitUntil(
+    caches.keys().then((keysArray) => {
+      return Promise.all(
+        keysArray.map((key) => {
+          if (
+            key !== currentStaticCacheName &&
+            key !== currentDynamicCacheName
+          ) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
+
+//dynamic cashe: ene 23ml cache l 2y 7aga el 7aga el user 3mlha fetch w heya msh mwgoda fl precache.
 
 //fetch
 self.addEventListener('fetch', (e) => {
@@ -38,12 +60,20 @@ self.addEventListener('fetch', (e) => {
       //lw e.request msh mwgod fl cach hykon el response null
       return (
         cacheRes ||
-        fetch(e.request).then((networkRes) => {
-          return caches.open('dynamic').then((dynamicCacheRes) => {
-            dynamicCacheRes.put(e.request.url, networkRes.clone());
-            return networkRes;
-          });
-        })
+        fetch(e.request)
+          .then((networkRes) => {
+            return caches.open(currentDynamicCacheName).then((dynamicCache) => {
+              //3mlna clone 3shan el response can be consumed only one time w momkn 23ks 25le el return hwa ele cloned
+              dynamicCache.put(e.request.url, networkRes.clone());
+              return networkRes;
+            });
+          })
+          .catch((err) => {
+            //hena feh moshkla lsa m7lnhash en hena bn2ol lw el fetch baz yb3t fallback page 3ala 2sas eno lw d5l el page deh offline w hwa msh 3mlha cashe , bs moomkn el fetch yboz lw online brdo lw bgeb json data mn ay 7eta msln 2w 2y 7aga 7slt bshkl 3am fl server 2w el network. fana kda ahb3tlo el fallback page brdo fe 2y wa2t wana 3yzha tkon bs lw hwa offline w m3mlsh ll page cashe.
+            return caches.open(currentStaticCacheName).then((staticCache) => {
+              return staticCache.match('/pwa%20test/offlineFallback.html');
+            });
+          })
       );
     })
   );
